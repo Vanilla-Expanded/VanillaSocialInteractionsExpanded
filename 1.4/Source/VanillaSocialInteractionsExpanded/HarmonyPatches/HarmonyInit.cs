27e7084aed65ef501775ed23e1f4836161c0744a
Taranchuk <nfhfyxer@gmail.com>
2022-09-18 19:57:35 +0300

    1.4 update

---
+++ 1.4/Source/VanillaSocialInteractionsExpanded/HarmonyPatches/HarmonyInit.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace VanillaSocialInteractionsExpanded
+{
+	[StaticConstructorOnStartup]
+	internal static class HarmonyInit
+	{
+		public static Harmony harmony;
+		static HarmonyInit()
+		{
+			harmony = new Harmony("OskarPotocki.VanillaSocialInteractionsExpanded");
+			harmony.PatchAll();
+		}
+	}
+}
